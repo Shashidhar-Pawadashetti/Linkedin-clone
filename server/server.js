6a5de932c9312d4f0bdfe7c53e729a310fc2ed3f
Shashidhar Pawadashetti <shashipshetti@gmail.com>
2025-12-05 22:43:10 +0530

    Implement Register Feature

--- server/server.js
+++ server/server.js
@@ -3,15 +3,16 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import postRoutes from "./routes/posts.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(postRoutes);
-
+app.use(userRoutes);
 connectDB();
 
 app.get("/", (req, res) => {
